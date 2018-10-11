87acc7edb9d1260f6c7af2b6c7842db6619e53d1
liuhx9988 <liuhx1122@gmail.com>
2018-10-10 23:00:19 -0700

    Add project files.

---
+++ Over Jumped/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Over_Jumped
+{
+    partial class Over_Jumped
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Timer Ver;
+            System.Windows.Forms.Timer Hor;
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.Test = new System.Windows.Forms.Label();
+            Ver = new System.Windows.Forms.Timer(this.components);
+            Hor = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ver
+            // 
+            Ver.Enabled = true;
+            Ver.Interval = 10;
+            Ver.Tick += new System.EventHandler(this.Ver_Tick);
+            // 
+            // Hor
+            // 
+            Hor.Enabled = true;
+            Hor.Interval = 10;
+            Hor.Tick += new System.EventHandler(this.Hor_Tick);
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            this.Player.Image = global::Over_Jumped.Properties.Resources.download;
+            this.Player.Location = new System.Drawing.Point(378, 228);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(142, 221);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // Test
+            // 
+            this.Test.AutoSize = true;
+            this.Test.Location = new System.Drawing.Point(167, 123);
+            this.Test.Name = "Test";
+            this.Test.Size = new System.Drawing.Size(0, 17);
+            this.Test.TabIndex = 1;
+            // 
+            // Over_Jumped
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 941);
+            this.Controls.Add(this.Test);
+            this.Controls.Add(this.Player);
+            this.Name = "Over_Jumped";
+            this.Text = "Over Jumped";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Over_Jumped_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Over_Jumped_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Label Test;
+    }
+}
+
---
+++ Over Jumped/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Over_Jumped
+{
+    public partial class Over_Jumped : Form
+    {
+        bool playerL = false;
+        bool playerR = false;
+        bool playerUp = false;
+        bool playerGound = false;
+        int JumpCounter = 0;
+
+        public Over_Jumped()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int Vspeed = 0;
+        bool alreadyJump = false;
+        private void Ver_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top >= this.Size.Height - Player.Height - 30)
+            {
+                Vspeed = 0;
+                playerGound =true;
+
+            }
+            //else if(playerGound)
+            //{
+            //    Vspeed = 0;
+            //}
+            else
+            {
+                Vspeed += 1;
+                playerGound = false;
+            }
+
+
+            if (!alreadyJump && playerUp && (JumpCounter <20|| playerGound))
+            {
+                Vspeed = -10;
+                JumpCounter++;
+                if (JumpCounter >= 20)
+                {
+                    alreadyJump = true;
+                }
+            }
+
+            else if (playerGound)
+            {
+                JumpCounter = 0;
+                if(!playerUp)
+                {
+                    alreadyJump = false;
+                }
+            }else if(!playerUp)
+            {
+                JumpCounter+=5;
+            }
+
+            if (Player.Top + Vspeed > this.Size.Height - Player.Height - 30)
+            {
+                Player.Top = this.Size.Height - Player.Height - 30;
+            }
+            else
+            {
+                Player.Top += Vspeed;
+            }
+
+
+        }
+
+
+        private void Hor_Tick(object sender, EventArgs e)
+        {
+            const int Hspeed = -5;
+            if (playerL)
+            {
+                Player.Left += Hspeed;
+                Test.Text = "Move left";
+            }
+
+            if (playerR)
+            {
+                Player.Left -= Hspeed;
+                Test.Text = "Move Right";
+            }
+
+        }
+
+        private void Over_Jumped_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    playerL = true;
+                    break;
+                case Keys.Right:
+                    playerR = true;
+                    break;
+                case Keys.Up:
+                    playerUp = true;
+                    break;
+            }
+        }
+
+        private void Over_Jumped_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    playerL = false;
+                    break;
+                case Keys.Right:
+                    playerR = false;
+                    break;
+                case Keys.Up:
+                    playerUp = false;
+                    break;
+            }
+        }
+    }
+}
